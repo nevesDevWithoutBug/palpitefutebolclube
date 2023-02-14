b85d6cd5b8ddf45714d6c8bd4f940c57c9dde8d0
Guilherme Venancio <guilherme.eufrasio@camptecnologia.com.br>
2023-02-14 10:07:32 -0300

    subindo dev

--- src/components/adminHome/index.tsx
+++ src/components/adminHome/index.tsx
@@ -14,7 +14,7 @@ function AdminHomeComponent() {
             <div onClick={() => setMenuStatus(!menuStatus)} className={style.divIconMenu}>
                 <svg fill="#e4e4e4" className={ style.buttonMenu } viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
             </div>
-            {menuStatus && <MenuComponent setMenu={setMenuSelected} />}
+            <MenuComponent setMenu={setMenuSelected} />
             <ContentComponent page={menuSelected} />
         </div>
     )
